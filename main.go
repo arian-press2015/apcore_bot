dd7dfda4b297ca842026665659d33fc2fb54bb2b
AP2015 <arian.press2015@gmail.com>
2024-07-10 23:04:21 +0330

    feat: write first webhook

--- main.go
+++ main.go
@@ -2,53 +2,86 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
 	botToken := "7312956632:AAGc_wP8qjEeJvByBV3falkqPdZ_hd9wxFY"
+	webhookURL := "cafe-ro.com/bot"
 
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	webhook, err := tgbotapi.NewWebhook(webhookURL + bot.Token)
+	if err != nil {
+		log.Panic(err)
+	}
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	_, err = bot.Request(webhook)
+	if err != nil {
+		log.Panic(err)
+	}
 
-	updates := bot.GetUpdatesChan(u)
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	http.ListenAndServe(":4000", nil)
 
 	for update := range updates {
 		if update.Message != nil {
-			switch update.Message.Command() {
-			case "start":
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome! Use the custom keyboard to navigate.")
-
-				keyboard := tgbotapi.NewReplyKeyboard(
-					tgbotapi.NewKeyboardButtonRow(
-						tgbotapi.NewKeyboardButton("Option 1"),
-						tgbotapi.NewKeyboardButton("Option 2"),
-					),
-				)
-
-				msg.ReplyMarkup = keyboard
-
-				bot.Send(msg)
-			case "help":
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Available commands:\n/start - start the bot\n/help - see this help message")
-				bot.Send(msg)
-			default:
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I don't know that command.")
-				bot.Send(msg)
-			}
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			bot.Send(msg)
 		}
 	}
 }
+
+// func main() {
+// 	botToken := "7312956632:AAGc_wP8qjEeJvByBV3falkqPdZ_hd9wxFY"
+
+// 	bot, err := tgbotapi.NewBotAPI(botToken)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+
+// 	bot.Debug = true
+
+// 	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+// 	u := tgbotapi.NewUpdate(0)
+// 	u.Timeout = 60
+
+// 	updates := bot.GetUpdatesChan(u)
+
+// 	for update := range updates {
+// 		if update.Message != nil {
+// 			switch update.Message.Command() {
+// 			case "start":
+// 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome! Use the custom keyboard to navigate.")
+
+// 				keyboard := tgbotapi.NewReplyKeyboard(
+// 					tgbotapi.NewKeyboardButtonRow(
+// 						tgbotapi.NewKeyboardButton("Option 1"),
+// 						tgbotapi.NewKeyboardButton("Option 2"),
+// 					),
+// 				)
+
+// 				msg.ReplyMarkup = keyboard
+
+// 				bot.Send(msg)
+// 			case "help":
+// 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Available commands:\n/start - start the bot\n/help - see this help message")
+// 				bot.Send(msg)
+// 			default:
+// 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I don't know that command.")
+// 				bot.Send(msg)
+// 			}
+// 		}
+// 	}
+// }
 
 // if update.Message != nil {
 // 	switch update.Message.Command() {
